b2d07ba0c0ec3bf073a10180931c9208d46b19aa
Viviane Guimarães <83373229+vivianeguimaraesdev@users.noreply.github.com>
2021-09-27 17:14:19 -0300

    Criação de projeto

---
+++ aula-03-04codelab/script.js
@@ -0,0 +1,27 @@
+
+let arrayJogosFavoritos= [
+    { 
+    nome: 'Red Dead Redeptiom 2',
+    imagem:'https://images5.alphacoders.com/917/917971.jpg' ,
+    genero: 'Ação',
+    nota: '10',
+    situacao:'true',
+    },
+    { 
+        nome: 'DOTA 2',
+        imagem:'https://thesquad.com.br/wp-content/uploads/2019/09/dota2-capa.jpg' ,
+        genero: 'MOBA',
+        nota: '9',
+        situacao:'true',
+        }
+]
+arrayJogosFavoritos.push()
+
+// função que renderiza os cards na tela 
+const renderCardsGames = (todo) =>{
+//mapeamento da lista onde será incluida a tarefa
+const list =document.querySelector('.js-listGames');
+
+}
+renderCardsGames('arrayJogosFavoritos');
+
